68a9e8692ed393f737bcb9fccaf685d5991f8c62
evtruong <146985216+evtruong@users.noreply.github.com>
2024-05-11 23:36:33 -0500

    ver 4.8

--- ethan/ethan-g.js
+++ ethan/ethan-g.js
@@ -1,8 +1,57 @@
 let x;
+let y;
 
-document.getElementById("rollDie").onclick = function(){
+var player = {
+    health = 100,
+    dmge = function(y){
 
-    x = Math.floor(Math.random() * 20) + 1;
+        y = Math.floor(Math.random() * 20) + 1;
 
-    document.getElementById("xlabel").innerHTML = x;
+        if (y <= 5){
+            alert("Dodged")
+        }
+        else if (y > 5 && y <=10){
+            this.health - 5;
+        }
+        else if (y > 10 && y <=15){
+            this.health - 10;
+        }
+        else if (y > 15 && y <= 19){
+            this.health - 15;
+        }
+        else {
+            this.health - 25;
+        }
+    }
 }
+
+var monster = {
+    health = 200,
+    dmge = function(x){
+
+        x = Math.floor(Math.random() * 20) + 1;
+
+        if (x <= 5){
+            alert("Dodged")
+        }
+        else if (x > 5 && x <=10){
+            this.health - 10;
+        }
+        else if (x > 10 && x <=15){
+            this.health - 15;
+        }
+        else if (x > 15 && x <= 19){
+            this.health - 25;
+        }
+        else {
+            this.health - 50;
+        }
+    }
+
+}
+
+var pRoll = document.getElementById("pDie").onclick;
+
+
+var monsterHealth = document.getElementById("monHp");
+monHp.textContent=monster.health;
