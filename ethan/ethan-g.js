9a379b14b35fe13474b1f7a94fe70c344c3f4b83
evtruong <146985216+evtruong@users.noreply.github.com>
2024-05-22 12:34:37 -0500

    ver 11.1

--- ethan/ethan-g.js
+++ ethan/ethan-g.js
@@ -1,10 +1,19 @@
 const playBoard = document.querySelector(".play-board");
 
-let foodX = 13, foodY = 10;
+let foodX, foodY;
+let snakeX = 5, snakeY = 10;
+
+const changeFoodPosition = () => {
+    // Passing a random 0 - 30 value as food position
+    foodX = Math.floor(Math.random() * 30) + 1;
+    foodY = Math.floor(Math.random() * 30) + 1;
+}
 
 const initGame = () => {
     let htmlMarkup = `<div class="food" style="grid-area: ${foodY} / ${foodX} "></div>`;
+    htmlMarkup += `<div class="head" style="grid-area: ${snakeY} / ${snakeX} "></div>`;
     playBoard.innerHTML = htmlMarkup;
 }
-initGame()
+changeFoodPosition();
+initGame();
 //grid-area is a shorthand property that sets values for grid item's start and endlines for both the row and column.
