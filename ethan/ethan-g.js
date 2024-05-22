b6d00c566379f380a34ff301a22d5ec274262560
evtruong <146985216+evtruong@users.noreply.github.com>
2024-05-22 12:10:10 -0500

    ver 10.8

---
+++ ethan/ethan-g.js
@@ -0,0 +1,10 @@
+const playBoard = document.querySelector("play-board");
+
+let foodX = 13, foodY = 10;
+
+const initGame = () => {
+    let htmlMarkup = `<div class="food" style="grid-area: ${foodY} / ${foodX} "></div>`;
+    playBoard.innerHTML = htmlMarkup;
+}
+initGame()
+//grid-area is a shorthand property that sets values for grid item's start and endlines for both the row and column.
