3140c35ec54210e9d6928e16cfbb348d2fd6f0b8
evtruong <146985216+evtruong@users.noreply.github.com>
2024-05-23 12:29:37 -0500

    ver 11.2

--- ethan/ethan-g.js
+++ ethan/ethan-g.js
@@ -1,7 +1,11 @@
 const playBoard = document.querySelector(".play-board");
 
+let gameOver = false;
 let foodX, foodY;
 let snakeX = 5, snakeY = 10;
+let snakeBody = []
+let velocityX = 0, velocityY = 0;
+let setIntervalId;
 
 const changeFoodPosition = () => {
     // Passing a random 0 - 30 value as food position
@@ -9,11 +13,70 @@ const changeFoodPosition = () => {
     foodY = Math.floor(Math.random() * 30) + 1;
 }
 
+const handleGameOver = () => {
+    clearInterval(setIntervalId);
+    alert("Game Over! Press OK to replay... ");
+    location.reload();
+}
+
+const changeDirection = (e) => {
+    // Changing velocity value based on key press
+    if(e.key === "ArrowUp" && velocityY != 1){
+        velocityX = 0;
+        velocityY = -1;
+    }
+    else if (e.key === "ArrowDown" && velocityY != -1){
+        velocityX = 0;
+        velocityY = 1;
+    }
+    else if (e.key === "ArrowLeft" && velocityX != 1){
+        velocityX = -1;
+        velocityY = 0;
+    }
+    else if (e.key === "ArrowRight" && velocityX != -1){
+        velocityX = 1;
+        velocityY = 0;
+    }
+}
+
 const initGame = () => {
+    if(gameOver) return handleGameOver();
     let htmlMarkup = `<div class="food" style="grid-area: ${foodY} / ${foodX} "></div>`;
-    htmlMarkup += `<div class="head" style="grid-area: ${snakeY} / ${snakeX} "></div>`;
+
+    if(snakeX === foodX && snakeY === foodY){
+        changeFoodPosition();
+        snakeBody.push([foodX, foodY]); // Pushing food position to snake body array
+        
+    }
+
+    for (let i = snakeBody.length - 1; i > 0; i--){
+        // Shifting forward the values of the elements in the snake body by one
+        snakeBody[i] = snakeBody[i-1];
+    }
+
+    snakeBody[0] = [snakeX, snakeY]; // Setting first element of snake body to current snake position
+
+    // Updating the snake's head position based on current velocity
+    snakeX += velocityX;
+    snakeY += velocityY;
+
+    // Checking if the snake's head is out of wall, if so setting gameOver to true
+    if(snakeX <= 0 || snakeX > 30 || snakeY <= 0 || snakeY > 30){
+        gameOver = true;
+    }
+
+    for (let i = 0; i < snakeBody.length; i++){
+        // Adding a div for each part of snake's body
+        htmlMarkup += `<div class="head" style="grid-area: ${snakeBody[i][1]} / ${snakeBody[i][0]} "></div>`;
+        // Checking if the snake head hit the body, if so set gameOver to true
+        if (i !== 0 && snakeBody[0][1] === snakeBody[i][1] && snakeBody[0][0] === snakeBody[i][0]) {
+            gameOver = true;
+        }
+    }
     playBoard.innerHTML = htmlMarkup;
 }
+
 changeFoodPosition();
-initGame();
+setIntervalId = setInterval(initGame, 125);
+document.addEventListener("keydown", changeDirection)
 //grid-area is a shorthand property that sets values for grid item's start and endlines for both the row and column.
