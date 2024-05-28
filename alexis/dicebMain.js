c8a9ca35966ed0d969a8e8afd4f3380b4c1214e6
AlexisPelaez <147647973+AlexisPelaez@users.noreply.github.com>
2024-05-28 17:32:58 -0500

    Images to my game

--- alexis/dicebMain.js
+++ alexis/dicebMain.js
@@ -228,42 +228,42 @@ var dice = {
 
     //Determines the image presented based on the roll (FOR THE USER)//
     if (result===1){
-      var dierolled = "diemedia/dice-1.jpg";
+      var dierolled = "alexis/media/dice-1.jpg";
     }
     else if (result===2){
-      var dierolled = "diemedia/dice-2.jpg";
+      var dierolled = "alexis/media/dice-2.jpg";
     }
     else if (result===3){
-      var dierolled = "diemedia/dice-3.jpg";
+      var dierolled = "alexis/media/dice-3.jpg";
     }
     else if (result===4){
-      var dierolled = "diemedia/dice-4.jpg";
+      var dierolled = "alexis/media/dice-4.jpg";
     }
     else if (result===5){
-      var dierolled = "diemedia/dice-5.jpg";
+      var dierolled = "alexis/media/dice-5.jpg";
     }
     else if (result===6){
-      var dierolled = "diemedia/dice-6.jpg";
+      var dierolled = "alexis/media/dice-6.jpg";
     }
 
     //Determines the image presented based on the roll (FOR THE ENEMY)//
       if (enemyresult===1){
-        var enemydierolled = "diemedia/dice-1.jpg";
+        var enemydierolled = "alexis/media/dice-1.jpg";
       }
       else if (enemyresult===2){
-        var enemydierolled = "diemedia/dice-2.jpg";
+        var enemydierolled = "alexis/media/dice-2.jpg";
       }
       else if (enemyresult===3){
-        var enemydierolled = "diemedia/dice-3.jpg";
+        var enemydierolled = "alexis/media/dice-3.jpg";
       }
       else if (enemyresult===4){
-        var enemydierolled = "diemedia/dice-4.jpg";
+        var enemydierolled = "alexis/media/dice-4.jpg";
       }
       else if (enemyresult===5){
-        var enemydierolled = "diemedia/dice-5.jpg";
+        var enemydierolled = "alexis/media/dice-5.jpg";
       }
       else if (enemyresult===6){
-        var enemydierolled = "diemedia/dice-6.jpg";
+        var enemydierolled = "alexis/media/dice-6.jpg";
       }
     //The if statement that allows the user to continue fighting
     function determineEnd(result){
@@ -415,8 +415,8 @@ var dice = {
     document.getElementById("user").innerHTML = "";
     document.getElementById("enemy").innerHTML = "";
     document.getElementById("resultofwinner").innerHTML = "";
-    document.querySelector("#userimg").setAttribute("src","diemedia/questionmark.jpg");
-    document.querySelector("#enemyimg").setAttribute("src","diemedia/questionmark.jpg");
+    document.querySelector("#userimg").setAttribute("src","alexis/media/questionmark.jpg");
+    document.querySelector("#enemyimg").setAttribute("src","alexis/media/questionmark.jpg");
     var completedOnce = "true"; //Change
     customPrompt(completedOnce);
   }
