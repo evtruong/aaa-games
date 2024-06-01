2f9cb6a30cdd5a734a7624d0e8fd632bbb16e22d
willrobbins1 <147647719+willrobbins1@users.noreply.github.com>
2024-05-31 23:41:36 -0500

    ver 16.7

--- reveiw.js
+++ reveiw.js
@@ -1,44 +1,26 @@
-var allu=[""]
-var allr=[""]
-var un=prompt("What is your user Name")
-function review(){
-    if(un!="bob"){
-        //Reveiws
-        let allr2=localStorage.getItem("3")
-        allr=JSON.parse(allr2)
-        let x = document.forms["form2"]["form1"].value;
-        allr.push(x)
-        let allr1= JSON.stringify(allr)
-        localStorage.setItem("3", allr1)
-        allr2=localStorage.getItem("3")
-        let allr3=JSON.parse(allr2)
-    }
-    else(
-        alert("You can't write more than one review with that user name")
-    )
-    document.getElementById("o").hidden=true
-
+const para=document.getElementsByTagName("p")
+const tfa=document.getElementsByTagName("td")
+const tha=document.getElementsByTagName("th")
+for(var i=0; i<para.length-1; i++){
+    para[i].className="jacquard-12-regular";
 }
-
-function tf(){
-    document.getElementById("o").hidden=false
-    document.getElementById("q").hidden=true
+for(var i=0; i<tfa.length; i++){
+    tfa[i].className="jersey-10-regular";
+    tfa[i].style.color="#123C69"
+    tfa[i].style.fontSize="2dvi"
+    tfa[i].style.marginTop="auto"
+    tfa[i].style.marginBottom="auto"
+    tfa[i].style.marginRight="auto"
+    tfa[i].style.marginLeft="auto"
 }
-function tfn(){
-    document.getElementById("o").hidden=true
-    document.getElementById("q").hidden=true
+for(var i=0; i<tha.length; i++){
+    tha[i].className="jacquard-12-regular"
+    tha[i].style.color="#Ac3b61"
+    tha[i].style.fontSize="x-large"
+    tfa[i].style.fontSize="1.6dvi"
+    tha[i].style.fontWeight="600"
+    tfa[i].style.marginTop="auto"
+    tfa[i].style.marginBottom="auto"
+    tfa[i].style.marginRight="auto"
+    tfa[i].style.marginLeft="auto"
 }
-//not funcitonal
-function usernamest(){
-    let allu2=localStorage.getItem("4")
-    allu=JSON.parse(allu2)
-    allu.push(un)
-    let allu1= JSON.stringify(allu)
-    localStorage.setItem("4", allu1)
-    allu2=localStorage.getItem("4")
-    let allu3=JSON.parse(allu2)
-    alert(allu3)
-    alert("bob")
-}
-
-usernamest()
